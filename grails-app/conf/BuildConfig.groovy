5174c3df1feb3c5abca26a336a8348290d58dd2e
Cláudio Luiz <barthy@gmail.com>
2015-08-05 09:28:26 -0300

    3a teste de persistencia (postgree no heroku)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,6 +63,9 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
+		//Tentando instalar o plugin do heroku - Claudio 05/08/2015
+		compile ':heroku:1.0.1'
+		compile ':cloud-support:1.0.11'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
